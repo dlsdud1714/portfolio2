67c097fe88d2955f05a791612157bd54b3ec204c
Inyoung Park <rkduddl1rl@gmail.com>
2022-07-14 01:53:37 -0600

    font bug

--- file/components/header/Layout.jsx
+++ file/components/header/Layout.jsx
@@ -10,11 +10,9 @@ const Layout = ({ children }) => {
         <title>IYP Portfolio</title>
         <meta name="description" content="Inyoung's portfolio" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Acme&family=Assistant:wght@200;300;400;500&family=Bebas+Neue&family=Kelly+Slab&family=Montserrat+Subrayada:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Acme&family=Assistant:wght@200;300;400;500&family=Bebas+Neue&family=Kelly+Slab&family=Montserrat+Subrayada:wght@400;700&display=swap" rel="stylesheet"/>
+        
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>

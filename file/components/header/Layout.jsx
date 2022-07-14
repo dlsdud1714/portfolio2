6d05c75216fe2faf62d84572c2d26a567418ca9c
Inyoung Park <rkduddl1rl@gmail.com>
2022-07-14 01:54:12 -0600

    2

--- file/components/header/Layout.jsx
+++ file/components/header/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
         <title>IYP Portfolio</title>
         <meta name="description" content="Inyoung's portfolio" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Acme&family=Assistant:wght@200;300;400;500&family=Bebas+Neue&family=Kelly+Slab&family=Montserrat+Subrayada:wght@400;700&display=swap" rel="stylesheet"/>
         
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>

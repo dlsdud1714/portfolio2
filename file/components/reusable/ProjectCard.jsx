4f7e43610bc7bf9efa195274ba35b5cbf3203775
InyoungPark <75140270+dlsdud1714@users.noreply.github.com>
2022-11-16 18:38:11 -0700

    Project - data organized with props

---
+++ file/components/reusable/ProjectCard.jsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import PercentageBar from "./PercentageBar";
+import styles from "../../styles/css/Projects.module.css";
+
+export const ProjectCard = (props) => {
+  const project = props.project;
+
+  return (
+    <div className={styles.text_body}>
+      <div className={styles.project_backgroud}></div>
+
+      <div className={styles.container_blue}>
+        <div className={styles.img_container}>
+          <Image src={project.img} layout="fill" alt="" />
+        </div>
+        <div className={styles.text_container}>
+          <div className={styles.first_row}>
+            <h2 className={styles.title_h2}>{project.name}</h2>
+            <div className={styles.text_details_container}>
+              <div className={styles.details}>
+                <p className={styles.title_h3}>DESCRIPTION</p>
+                <p className={styles.text}>: {project.description}</p>
+              </div>
+              <div className={styles.details}>
+                <p className={styles.title_h3}>STACK</p>
+                <p className={styles.text}>
+                  :{" "}
+                  {project.stack.map((e, i) => {
+                    if (i == project.stack.length - 1) {
+                      return <span key={i}>and {e}</span>;
+                    }
+
+                    return <span key={i}>{e}, </span>;
+                  })}
+                </p>
+              </div>
+              <div className={styles.details_contribution}>
+                <p className={styles.title_h3}>MY CONTRIBUTION</p>
+                <div className={styles.percentageBar_container}>
+                  <PercentageBar
+                    color="white"
+                    frontEnd={project.myContribution[0]}
+                    styling={project.myContribution[1]}
+                    backEnd={project.myContribution[2]}
+                  />
+                </div>
+              </div>
+              <div className={styles.details}>
+                <p className={styles.title_h3}>DURATION</p>
+                <p className={styles.text}>: {project.duration} months</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.second_row}>
+            <div className={styles.second_row}>
+              <Link href={project.link}>
+                <a className={styles.button_container}>
+                  <button className={styles.button}>
+                    Learn more about this project
+                  </button>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- file/components/sections/Projects.js
+++ file/components/sections/Projects.js
@@ -1,157 +1,47 @@
-import Image from "next/image";
 import React from "react";
-import PercentageBar from "../reusable/PercentageBar";
-// import project2 from "../../utilities/project2_main.png";
-// import project3 from "../../utilities/project3_main.png";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+import project2 from "../../public/picture/project2_main.png";
+import project3 from "../../public/picture/project3_main.png";
 import styles from "../../styles/css/Projects.module.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Link from "next/link";
+import { ProjectCard } from "../reusable/ProjectCard";
 // import 'swiper/css/scrollbar';
 
 const projects = () => {
-  const project2 = '/picture/project2_main.png'
-  const project3 = '/picture/project3_main.png'
-  
+
+  const data = [
+    {
+      name: "Employee Scheduling System",
+      img:project3,
+      description:
+        "Employee schedules management app providing calendar, scheduling, messaging, and shift swap.",
+      stack: ['React', 'JavaScript', 'CSS', 'Express', 'MUI', 'mySQL'],
+      myContribution:[40,35,25],
+      duration:3,
+      link:"/projects/winkScheduling"
+    },{
+      name: "Calgary Local Business Website",
+      img: project2,
+      description:
+      "Interactive platform for calgary local businesses suffered insufficient budgets for marketing.",
+      stack:['React', 'JavaScript', 'CSS', 'Express', 'Bootstrap', 'MongoDB'],
+      myContribution: [45,25,35],
+      duration:1.5,
+      link:"/projects/localBusiness"
+    }
+  ];
   return (
     <div className={styles.contents} id="projects">
       <div className={styles.title_container}>
         <h1 className={styles.title_h1}>projects</h1>
         <div className={styles.title_background}></div>
       </div>
-      <div className={styles.text_body}>
-        <div className={styles.project_backgroud}></div>
-        <Swiper
-          pagination={true}
-          loop={true}
-          // navigation={true}
-          modules={[Pagination]}
-          // className={styles.mySwiper}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className={styles.container_blue}>
-              <div className={styles.img_container}>
-                <Image src={project3} layout="fill" />
-              </div>
-              <div className={styles.text_container}>
-                <div className={styles.first_row}>
-                  <h2 className={styles.title_h2}>
-                    Employee scheduling system
-                  </h2>
-                  <div className={styles.text_details_container}>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>DESCRIPTION</p>
-                      <p className={styles.text}>
-                        : Employee schedules management app providing calendar,
-                        scheduling, messaging, and shift swap.
-                      </p>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>STACK</p>
-                      <p className={styles.text}>
-                        : React, JavaScript, CSS, Express, MUI and mySQL
-                      </p>
-                    </div>
-                    <div className={styles.details_contribution}>
-                      <p className={styles.title_h3}>MY CONTRIBUTION</p>
-                      <div className={styles.percentageBar_container}>
-                        <PercentageBar
-                          color="white"
-                          frontEnd={40}
-                          styling={35}
-                          backEnd={25}
-                        />
-                      </div>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>DURATION</p>
-                      <p className={styles.text}>: 3 months</p>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>GROUP</p>
-                      <p className={styles.text}>: 3 (+ with Wink)</p>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.second_row}>
-                <div className={styles.second_row}>
-                  <Link href="/projects/winkScheduling">
-                    <a className={styles.button_container}>
-                    <button className={styles.button}>
-                      Learn more about this project
-                    </button>
-                    </a>
-                  </Link>
-                </div>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.container_blue}>
-              <div className={styles.img_container}>
-                <Image src={project2} layout="fill" alt="" />
-              </div>
-              <div className={styles.text_container}>
-                <div className={styles.first_row}>
-                  <h2 className={styles.title_h2}>
-                    Calgary Local Business website
-                  </h2>
+      {data.map((d,i)=>{
 
-                  <div className={styles.text_details_container}>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>DESCRIPTION</p>
-                      <p className={styles.text}>
-                        : Interactive platform for calgary local businesses
-                        suffered insufficient budgets for marketing.
-                      </p>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>STACK</p>
-                      <p className={styles.text}>
-                        : React, JavaScript, CSS, Bootstrap, Express, and Mongo
-                        DB
-                      </p>
-                    </div>
-                    <div className={styles.details_contribution}>
-                      <p className={styles.title_h3}>MY CONTRIBUTION</p>
-                      <div className={styles.percentageBar_container}>
-                        <PercentageBar
-                          color="white"
-                          frontEnd={45}
-                          styling={20}
-                          backEnd={35}
-                        />
-                      </div>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>DURATION</p>
-                      <p className={styles.text}>: 1.5 months</p>
-                    </div>
-                    <div className={styles.details}>
-                      <p className={styles.title_h3}>GROUP</p>
-                      <p className={styles.text}>: 3</p>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.second_row}>
-                  <Link href="/projects/localBusiness">
-                    <a className={styles.button_container}>
-                    <button className={styles.button}>
-                      Learn more about this project
-                    </button>
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+       return <ProjectCard key={i} project={d}/>
+      })
+} 
     </div>
   );
 };
